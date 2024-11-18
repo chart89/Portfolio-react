d4b25530491200fa846250a794309ba88d6d19c4
Dariusz Biernat <kaiju83@outlook.com>
2024-11-18 22:37:12 +0100

    Add sidebar and its animation

---
+++ src/components/Test.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+const Test = () => {
+  const variants = {
+    visible: {
+      opacity: 1,
+      x: 100,
+      transition: { staggerChildren: 1 },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  const items = ["items1", "items2", "items3", "items4"];
+
+  return (
+    <div className="course">
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item) => (
+          <motion.li variants={variants} key={item}>
+            {item}
+          </motion.li>
+        ))}
+      </motion.ul>
+    </div>
+  );
+};
+
+export default Test;
